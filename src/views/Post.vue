d49435800ee65132908c39f8ee1b1669869f486d
GiovanniAS <blasgivx@gmail.com>
2022-04-16 15:21:51 -0500

    post vuetify

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,30 +1,30 @@
 <template>
-    <div class="container">
+    <v-conatainer>
         <form class="formPost" @submit.prevent="agregarProducto(producto)">
             <h3 class=""> Añadir producto al inventario</h3>
-            <div class="">
-                <input class="elementsi" type="text" placeholder="Ingresa el modelo" v-model="producto.nombre">
-            </div>
-            <div class="">
-                <input class="elementsi" type="text" placeholder="Ingresa la descripcion" v-model="producto.descripcion">
-            </div>
-            <div class="" placeholder="Ingresa el la categoria">
-                <input class="elementsi" type="text" placeholder="Ingresa el precio" v-model="producto.precio">
-            </div>
-            <div class="">
-                <b-form-select v-model="producto.categoria" :options="options" class="elementsi"></b-form-select>
+            <v-text-field class="elementsi" type="text" placeholder="Ingresa el modelo" v-model="producto.nombre">
+                <!-- <input class="elementsi" type="text" placeholder="Ingresa el modelo" v-model="producto.nombre"> -->
+            </v-text-field>
+            <v-text-field class="elementsi" type="text" placeholder="Ingresa la descripcion" v-model="producto.descripcion">
+                
+            </v-text-field>
+            <v-text-field class="elementsi" type="text" placeholder="Ingresa el precio" v-model="producto.precio">
+                
+            </v-text-field>
+
+            <v-select v-model="producto.categoria" :items="options" class="elementsi"></v-select>
                 <!-- <input type="text" placeholder="Ingresa el precio" class="form-control-my-2 elementsi" v-model="producto.precio"> -->
-            </div>
-            <b-button class="elements" type="submit">
+
+            <v-btn class="elements" type="submit">
                 Añadir
-            </b-button>
-            <router-link 
+            </v-btn>
+            <v-btn 
             class="elements"
             to="/productos">
             Cancelar
-            </router-link>
+            </v-btn>
         </form>
-    </div> 
+    </v-conatainer> 
 </template>
 
 <script>
