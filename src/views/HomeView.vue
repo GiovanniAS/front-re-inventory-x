16400a3af2e05df46d7370e5389bd52da095d643
GiovanniAS <blasgivx@gmail.com>
2022-05-24 13:49:30 -0500

    update homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,32 @@
 <template>
 <div>
-  <img alt="Vue logo" src="../assets/logo.png">
-    <form @submit.prevent="login(usuario)">
-      <input type="text" placeholder="Ingresa email" v-model="usuario.email">
-      <input type="password" placeholder="Ingresa contraseña" v-model="usuario.password">
-      <button type="submit">Ingresar</button>
-    </form>
+  <Header />
+    <v-app>
+      <v-card class="mx-10 my-10">
+        <v-app-bar dark color="blue">
+          <v-toolbar-title>Iniciar Sesión</v-toolbar-title>
+        </v-app-bar>
+        <v-card-text>
+          <form @submit.prevent="login(usuario)" class="v-form">
+            <v-row>
+              <v-col cols="12">
+                <v-text-field type="text" label="Email" v-model="usuario.email" required></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field type="password" label="Contraseña" v-model="usuario.password" required></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
+                <v-btn dark color="blue" type="submit">Ingresar</v-btn>
+              </v-col>
+            </v-row>
+          </form>
+        </v-card-text>
+      </v-card>
+    </v-app>
 </div>
   
 </template>
